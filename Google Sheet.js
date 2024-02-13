40672a8eb7330981776c512c2ca9c4fd1d25a92b
them1n9 <manikanta9k7@gmail.com>
2024-02-13 20:26:57 +0530

    first commit

---
+++ Google Sheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzJXXKBrW-Jl3rSFmgU5tmBQDZVDmIM1_E0dPqegFNLAO95pycwRsVLYUGjHYuTdaM/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
